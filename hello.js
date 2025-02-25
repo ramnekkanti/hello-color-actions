f082a407bfb7ae382e6f1abcd3d7ada3a2444e48
Ram Nekkanti <ramn@atlanticpkg.com>
2025-02-24 23:46:38 -0500

    Restored

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from my HW submission for Module 4.2!";
+const hello = () => "Hello World from the Office Hours in the terminal!";
 console.log(hello());
 exports.hello = hello;
